3dd0de602bc89aca473b329eea34c7e6bcfbb0ee
Santiago Quintero <santiagoquija123@gmail.com>
2023-02-11 10:31:43 -0500

    2nd commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { ContratosComponent } from './pages/contratos/contratos.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'cards',
+    component:CardsComponent
+  },
+  {
+    path:'contratos',
+    component:ContratosComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+import { HomeComponent } from './pages/home/home.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { ContratosComponent } from './pages/contratos/contratos.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardsComponent,
+    ContratosComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
